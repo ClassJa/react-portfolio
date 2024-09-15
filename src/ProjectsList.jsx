5ea96b3d209e74423e9511dca0be9267afba9998
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-14 21:09:54 -0500

    Added template for project list component

---
+++ src/ProjectsList.jsx
@@ -0,0 +1,9 @@
+function ProjectsList() {
+    return (
+        <ul>
+            {/* insert references to the different portfolio projects */}
+        </ul>
+    )
+}
+
+export default ProjectsList
