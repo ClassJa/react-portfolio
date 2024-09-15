b1e42017ea3df6355e6acd9bee2e8e6f59138695
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-14 23:01:26 -0500

    Added components

---
+++ src/components/NavList.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+function NavList() {
+const [input, setInput] = useState('')
+
+const handleChange = (e) => {
+    setInput(e.target.value);
+  };
+
+
+<aside>
+    <ul>
+        <li className='ul-no-bullet'>
+        <a onClick={handleChange}> About Me </a>
+        </li>
+        <li className='ul-no-bullet'>
+        <a onClick={handleChange}> Projects </a>
+        </li>
+        <li className='ul-no-bullet'>
+        <a onClick={handleChange}> Contact Me </a>
+        </li>
+    </ul>
+    </aside>
+}
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,14 @@
+<aside>
+    <ul>
+        <li className='ul-no-bullet'>
+        About Me
+        <a href="#"> About Me </a>
+        </li>
+        <li className='ul-no-bullet'>
+        <a href="#"> Projects </a>
+        </li>
+        <li className='ul-no-bullet'>
+        <a href="#"> Contact Me </a>
+        </li>
+    </ul>
+</aside>
