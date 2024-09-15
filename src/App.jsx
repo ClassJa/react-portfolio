863ebd8646acb86d532bdf7c96409fd28999c09f
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-14 23:07:43 -0500

    Fixed the import statement

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './components/NavList'
-import { About, ContactMe, Navigation, NavList, Projects, ProjectsList } from './components'
+// import { About, ContactMe, Navigation, NavList, Projects, ProjectsList } from './components'
+import './components'
 import './App.css'
 
 
