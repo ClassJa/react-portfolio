e0ee1db38a7e573b06ace030f07dcad49ef35a4b
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-18 19:57:31 -0500

    Debugging the layout of the header section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 function Footer() {
     return ( 
-   <footer>
-        <button className="links-footer"> Github </button> 
-        <button className="links-footer"> Resume </button> 
+   <footer id="position-links">
+    <p className="text-footer">Made by Julia A. </p>
+        <a href="https://github.com/ClassJa" target="blank"> <button className="links-footer"> Github </button> </a>
+        <a href=""> <button className="links-footer" target="blank"> Resume </button> </a>
     </footer>
     )
 }
--- src/components/HeaderTop.jsx
+++ src/components/HeaderTop.jsx
@@ -1,8 +1,22 @@
 function HeaderTop() {
     return ( 
-        <header>
+        <div>
+        <header className="header-words">
             Julia A's Portfolio 
+        <section className="nav-links">
+        <div className="nav-style">About Me</div>
+        <div className="nav-style">Portfolio</div>
+        <div className="nav-style">Contact Me</div>
+        <div className="nav-style"> Resume</div>
+        </section>
         </header>
+        {/* <section className="nav-links">
+        <div className="nav-style">About Me</div>
+        <div className="nav-style">Portfolio</div>
+        <div className="nav-style">Contact Me</div>
+        <div className="nav-style"> Resume</div>
+        </section> */}
+        </div>
     )
 }
 
