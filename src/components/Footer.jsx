4e346e97ab7cbe5599eec917109024d8a69e3a7c
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-19 14:03:38 -0500

    header responsiveness

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ function Footer() {
    <footer id="position-links">
     <p className="text-footer">Made by Julia A. </p>
         <a href="https://github.com/ClassJa" target="blank"> <button className="links-footer"> Github </button> </a>
-        <a href=""> <button className="links-footer" target="blank"> Resume </button> </a>
+        <a href="Assets/web-dev-resume.pdf" target="blank"> <button className="links-footer"> Resume </button> </a>
     </footer>
     )
 }
