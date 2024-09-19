0a35b717aaf9fdf4ea35a6f92a429e42da5fc7f3
JA <69608172+ClassJay@users.noreply.github.com>
2024-09-18 20:08:46 -0500

    Styling

--- src/components/HeaderTop.jsx
+++ src/components/HeaderTop.jsx
@@ -4,10 +4,10 @@ function HeaderTop() {
         <header className="header-words">
             Julia A's Portfolio 
         <section className="nav-links">
-        <div className="nav-style">About Me</div>
-        <div className="nav-style">Portfolio</div>
-        <div className="nav-style">Contact Me</div>
-        <div className="nav-style"> Resume</div>
+        <a className="anchor-decor" href="/about"> <div className="nav-style">About Me</div> </a>
+        <a className="anchor-decor" href="/projects"> <div className="nav-style">Portfolio</div> </a>
+        <a className="anchor-decor" href="/contact"> <div className="nav-style">Contact Me</div> </a>
+        <a className="anchor-decor" href="/resume"> <div className="nav-style"> Resume</div> </a>
         </section>
         </header>
         {/* <section className="nav-links">
